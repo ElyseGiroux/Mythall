e492525b6fdfcd8fc5fa4fd2d5d27726fe02f510
ElyseGiroux <info@elysegiroux.com>
2023-02-21 16:45:22 -0500

    Trying to resolve Cannot convert object to primitive value issue on vercel

--- server/utils/firebase.ts
+++ server/utils/firebase.ts
@@ -4,9 +4,9 @@ import { getFirestore } from "firebase-admin/firestore";
 const config = useRuntimeConfig();
 const serviceAccount = JSON.parse(config.FIREBASE);
 
-export const app = initializeApp({
+export const firebaseApp = initializeApp({
   credential: cert(serviceAccount),
   databaseURL: config.FIREBASEURL,
 });
 
-export const firestore = getFirestore();
+export const firestore = getFirestore(firebaseApp);
