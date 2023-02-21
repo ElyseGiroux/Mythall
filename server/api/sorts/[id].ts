90434d4f63593cc0010afef889c60812d3385ba9
ElyseGiroux <info@elysegiroux.com>
2023-02-21 16:59:32 -0500

    build error again...

--- server/api/sorts/[id].ts
+++ server/api/sorts/[id].ts
@@ -1,6 +1,19 @@
-import { firestore } from "../../utils/firebase";
+import { cert, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+// import { firestore } from "../../utils/firebase";
 
 export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const serviceAccount = JSON.parse(config.FIREBASE);
+
+  const app = initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: config.FIREBASEURL,
+  });
+
+  const firestore = getFirestore(app);
+
   const params = event.context.params;
   if (params && params.id) {
     const snap = await firestore.collection("sorts").doc(params.id).get();
--- server/api/sorts/index.get.ts
+++ server/api/sorts/index.get.ts
@@ -1,6 +1,19 @@
-import { firestore } from "../../utils/firebase";
+import { cert, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+//import { firestore } from "../../utils/firebase";
 
 export default defineEventHandler(async () => {
+  const config = useRuntimeConfig();
+  const serviceAccount = JSON.parse(config.FIREBASE);
+
+  const app = initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: config.FIREBASEURL,
+  });
+
+  const firestore = getFirestore(app);
+
   const snap = await firestore.collection("sorts").limit(9).get();
   return snap.docs.map((snap) => {
     return {
