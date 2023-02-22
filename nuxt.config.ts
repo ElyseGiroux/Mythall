b1c411cf404bf1a52232688c0d34a090754781ad
ElyseGiroux <info@elysegiroux.com>
2023-02-22 11:08:53 -0500

    Testing different server config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "vercel-edge",
-    prerender: {
-      crawlLinks: true,
-      routes: ["/"],
-    },
-  },
-  vite: {
-    define: {
-      __INTLIFY_PROD_DEVTOOLS__: false, // Fix a bug with vercel functions
-    },
-  },
+  // nitro: {
+  //   preset: "vercel-edge",
+  //   prerender: {
+  //     crawlLinks: true,
+  //     routes: ["/"],
+  //   },
+  // },
+  // vite: {
+  //   define: {
+  //     __INTLIFY_PROD_DEVTOOLS__: false, // Fix a bug with vercel functions
+  //   },
+  // },
   runtimeConfig: {
     FIREBASE: process.env.FIREBASE,
     FIREBASEURL: process.env.FIREBASEURL,
