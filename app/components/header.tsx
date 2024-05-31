28d899389df63908c223095f0e3d0154b20a0af1
ElyseGiroux <info@elysegiroux.com>
2024-05-30 20:04:51 -0400

    Fixed TS errors

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -87,7 +87,9 @@ export default function Header() {
         <PopoverGroup className="hidden lg:flex lg:gap-x-12 z-10">
           {links.map((link) =>
             link.dropdown ? (
-              <Popover className="relative z-10">
+              <Popover
+                key={link.name}
+                className="relative z-10">
                 <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white hover:text-accent transition z-10">
                   {link.name}
                   <ChevronDownIcon
@@ -118,6 +120,7 @@ export default function Header() {
               </Popover>
             ) : (
               <Link
+                key={link.name}
                 href={link.href}
                 target={link.target ? link.target : ""}
                 className="text-sm font-semibold leading-6 text-white hover:text-accent transition">
@@ -175,6 +178,7 @@ export default function Header() {
                 {links.map((link) =>
                   link.dropdown ? (
                     <Disclosure
+                      key={link.name}
                       as="div"
                       className="-mx-3">
                       {({ open }) => (
@@ -203,6 +207,7 @@ export default function Header() {
                     </Disclosure>
                   ) : (
                     <Link
+                      key={link.name}
                       href={link.href}
                       target={link.target ? link.target : ""}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:text-accent transition">
--- app/generateur/page.tsx
+++ app/generateur/page.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import Image from "next/image";
-
->>>>>>> b184650 (Fixed prod build error)
 export default function Home() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
